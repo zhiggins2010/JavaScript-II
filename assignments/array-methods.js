ae047826bb645398eef81392c912326efe538de0
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-09-17 17:03:31 -0600

    completed challenge 3 of array-methods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -66,11 +66,21 @@ console.log(fullNames);
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
 let firstNamesAllCaps = [];
+runners.map(uppercase =>{
+  firstNamesAllCaps.push(`${uppercase.first_name.toUpperCase()} , ${uppercase.last_name}`);
+});
 console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
 let runnersLargeSizeShirt = [];
+const largeShirtFilter = runners.filter(size => {
+  if (size.shirt_size === "L") {
+    runnersLargeSizeShirt.push(size);
+  }
+});
+
+
 console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
